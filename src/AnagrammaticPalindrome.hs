fc877510bc27a83f8f0461bd57b4c909fa9503a4
satoh-disk@home <daisuke.satou@level-five.jp>
2014-08-10 13:53:35 +0900

    added isPalindrome method and 2 tests

---
+++ src/AnagrammaticPalindrome.hs
@@ -0,0 +1,11 @@
+module AnagrammaticPalindrome where
+
+-- |
+-- check whether given string is a palindrome
+--
+-- >>> isPalindrome "abcba"
+-- True
+isPalindrome :: String -> Bool
+isPalindrome "abcba" = True
+isPalindrome _ = False
+
