be7c4c07c3707f90529d826cc67c81bb5a187d24
satoh-disk@home <daisuke.satou@level-five.jp>
2014-08-10 15:29:06 +0900

    added isAnagrammaticPalindrome method and 3 tests

--- src/AnagrammaticPalindrome.hs
+++ src/AnagrammaticPalindrome.hs
@@ -1,11 +1,22 @@
 module AnagrammaticPalindrome where
 
+import Data.List
+
 -- |
 -- check whether given string is a palindrome
 --
 -- >>> isPalindrome "abcba"
 -- True
-isPalindrome :: String -> Bool
-isPalindrome "abcba" = True
-isPalindrome _ = False
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == (reverse xs)
 
+-- |
+-- check whether given string is a anagrammatic palindrome
+--
+-- >>> isAnagrammaticPalindrome "abcba"
+-- True
+isAnagrammaticPalindrome :: (Eq a) => [a] -> Bool
+isAnagrammaticPalindrome xs = isContainPalindrome $ permutations xs
+    where isContainPalindrome [] = False
+          isContainPalindrome [y] = isPalindrome y
+          isContainPalindrome (y:ys) = (isPalindrome y) || (isContainPalindrome ys)
--- test/AnagrammaticPalindromeSpec.hs
+++ test/AnagrammaticPalindromeSpec.hs
@@ -9,7 +9,19 @@ import Test.Hspec
 spec :: Spec
 spec = do
     describe "isPalindrome" $ do
-        it "isPalindrome abcba returns True" $
-            isPalindrome "abcba" `shouldBe` True
         it "isPalindrome abc returns False" $
             isPalindrome "abc" `shouldBe` False
+        it "isPalindrome abcba returns True" $
+            isPalindrome "abcba" `shouldBe` True
+        it "isPalindrome wasitacatisaw returns True" $
+            isPalindrome "wasitacatisaw" `shouldBe` True
+        it "isPalindrome WasItACatISaw returns False" $
+            isPalindrome "WasItACatISaw" `shouldBe` False
+
+    describe "isAnagrammaticPalindrome" $ do
+        it "isAnagrammaticPalindrome aaa returns True" $
+            isAnagrammaticPalindrome "aaa" `shouldBe` True
+        it "isAnagrammaticPalindrome aab returns True" $
+            isAnagrammaticPalindrome "aab" `shouldBe` True
+        it "isAnagrammaticPalindrome abc returns False" $
+            isAnagrammaticPalindrome "abc" `shouldBe` False
